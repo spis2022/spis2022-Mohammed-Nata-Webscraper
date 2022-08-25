1c69fd8c44b4c35163c4800a5f0b51d733d544d0
Nata <99166243+Anastasiya-Ver@users.noreply.github.com>
2022-08-25 20:50:36 +0000

    -added logins -finished individual scraping functions -reverted to working but ugly image display

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import session
+from flask import url_for
+from flask import g
 from scraper import getLinks
+from flask_github import GitHub
+from flask_session import Session
+import os
 app = Flask(__name__)
-# dictionary for unique function
 
+# setting up github login
+app.config['GITHUB_CLIENT_ID'] = os.environ['GITHUB_CLIENT_ID']
+app.config['GITHUB_CLIENT_SECRET'] = os.environ['GITHUB_CLIENT_SECRET']
+github = GitHub(app)
 @app.route('/')
 def render_home():
     return render_template('home.html')
+# github login stuff
+@app.route('/login')
+def login():
+    return github.authorize()
+  
+@app.route('/callback')
+@github.authorized_handler
+def authorized(oauth_token):
+    if oauth_token is None:
+        return "Authorization Failed"
+
+    else:
+      return "Authorization Succeeded"
 
 @app.route('/scraper')
 def render_scraper():
   links = getLinks()
   return render_template('scraper.html', links = links)
-           
+
+
 if __name__ == "__main__":
-  app.run(host='0.0.0.0')
+ app.run(host='0.0.0.0')
--- scraper.py
+++ scraper.py
@@ -3,39 +3,66 @@ from bs4 import BeautifulSoup
 from bs4 import SoupStrainer
 
 # Setting up soup for the general area search page
-
 def getLinks():
- url = requests.get('https://garagesalefinder.com/yard-sales/sammamish-wa/')
- soup = BeautifulSoup(url.text,'html.parser')
-
-adtitles = soup.find("div", class_="small-9 columns").find_all("h2")
-for title in adtitles: 
-   print(title.text)
-  
-  # Will extract the links to garage sales from the website and put them into the list linkList
-  # I think this is missing some links by only saving ones ending in gallery, figure out why not all links have gallery and if things will still work if I slap it on
-links = {}  
-for aTag in soup.find_all('a', {'href': True}):
-     link = (aTag.get('href'))
-if 'garagesalefinder' in link and 'gallery' in link:
-      links[link] = {}
-     
-  # Will extract the corresponding title for each list in the same matching order
-# linkToTitle = {}
-  
-# titleList = []
-  
-  # Setting up subsidiary soup of the first garage sale on the prior list
-  # Intend to either make this a function or put it in a for loop so we can store all this info for every garage sale, but that's for later
-  for link in links.keys():
-    url2 = requests.get(str(link))
-    soup2 = BeautifulSoup(url2.text,'html.parser')
-  # Extracting the images and saving them to imageList
-
+  # soup for listing page
+  url = requests.get('https://garagesalefinder.com/yard-sales/sammamish-wa/')
+  soup = BeautifulSoup(url.text,'html.parser')
+  # collects links of listings
+  outerDict = {}  
+  for aTag in soup.find_all('a', {'href': True}):
+    link = (aTag.get('href'))
+    if 'garagesalefinder' in link and 'gallery' not in link:
+      outerDict[link] = {}
+  # building inner dictionary     
+  for link in outerDict.keys():
+    urlDescription = requests.get(str(link))
+    soupD = BeautifulSoup(urlDescription.text, 'html.parser')
+    # Extracting the images and saving them to imageList
+    # BELOW IS FOR IMAGES ONLY
+    linkGallery = link + "/gallery"
+    urlGallery = requests.get(str(linkGallery))
+    soupGallery = BeautifulSoup(urlGallery.text,'html.parser')
     imageList = []
-  
-    for images in soup2.find_all('img', {'data-lazy':True}):
+    for images in soupGallery.find_all('img', {'data-lazy':True}):
       imageList.append(images.get('data-lazy'))
-    links[link]['images']=imageList
-  return links
+      outerDict[link]['images']=imageList
+  return outerDict
 
+def getTitle():
+  url = requests.get("https://garagesalefinder.com/s/NUcGv/15326-corliss-pl-n-shoreline-wa-98133")
+  soup = BeautifulSoup(url.text, 'html.parser')
+  titleList = []
+  for title in soup.find_all('h2'):
+    titleList.append(title.text)
+  title = titleList[0]
+  return title
+
+def getAddress():
+  # find address here
+  url = requests.get("https://garagesalefinder.com/s/NUcGv/15326-corliss-pl-n-shoreline-wa-98133")
+  soup = BeautifulSoup(url.text, 'html.parser')
+  AddressList = []
+  for address in soup.find_all('h1'):
+    AddressList.append(address.text)
+  address = AddressList[0]
+  return address
+
+def getDescription():
+  url = requests.get("https://garagesalefinder.com/s/NUcGv/15326-corliss-pl-n-shoreline-wa-98133")
+  soup = BeautifulSoup(url.text, 'html.parser')
+  DescriptionList = []
+  for description in soup.find_all('p'):
+    DescriptionList.append(description.text)
+  description = DescriptionList[5]
+  return description.strip()
+
+def getDate():
+  url = requests.get("https://garagesalefinder.com/s/NUcGv/15326-corliss-pl-n-shoreline-wa-98133")
+  soup = BeautifulSoup(url.text, 'html.parser')
+  datesList = []
+  # might be a problem for implementing everything together, ideally just want to replace the tags in the "times" div section
+  for tag in soup.find_all('br'):
+    tag.replace_with(tag.text + ' ')
+  for times in soup.find_all('div', {"class": "date-time"}):
+    datesList.append((times.text).strip())
+  return datesList
