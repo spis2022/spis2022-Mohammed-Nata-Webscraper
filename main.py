b366412ca6ca5ece9504167e3989430f7faeda60
Nata <99166243+Anastasiya-Ver@users.noreply.github.com>
2022-08-29 23:30:13 +0000

    - added login/logout/register system ^^ taken from (https://medium.com/codex/simple-registration-login-system-with-flask-mongodb-and-bootstrap-8872b16ef915) -- track login or logout time -- make /listings dependent on the user (with a default guest view)

--- main.py
+++ main.py
@@ -7,11 +7,7 @@ from flask import Flask, url_for, render_template, request
 from flask import redirect
 from flask import session
 from pymongo import MongoClient
-# from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy.sql import text
-# from sqlalchemy import MetaData
-# from sqlalchemy.orm import joinedload
-# from flask_pymongo import PyMongo
+import bcrypt
 app = Flask(__name__)
 
 
@@ -30,9 +26,9 @@ github = GitHub(app)
 
 app.secret_key = os.environ['APP_SECRET_KEY']
 
-@app.route('/')
-def render_home():
-    return render_template('home.html')
+# @app.route('/')
+# def render_home():
+#     return render_template('home.html')
 
 #refreshes database with new info from scraper
 @app.route('/load-db')
@@ -55,39 +51,86 @@ def render_load_db():
       print("db deleted", entry)
   return "db loaded"
 
-@github.access_token_getter
-def token_getter():
-    user = g.user
-    if user is not None:
-        print(user.github_access_token)
-
-@app.route('/login')
-def login():
-    return github.authorize()
-  
-@app.route('/callback')
-@github.authorized_handler
-def authorized(oauth_token):
-    if oauth_token is None:
-      return "Authorization Failed"
-    else:
-      return oauth_token
-
 @app.route('/listings')
 def render_listings():
   listingsCursor = db.listings.find()
   return render_template('listings.html', cursor = listingsCursor)
   pass
-@app.route('/collapsingtest')
-def render_collapse():
-  return render_template('collapseTest.html')
 
-listingsCursor = db.listings.find()
-# for listing in listingsCursor:
-#   print(type(listing))
-#   print(len(listing.keys()))
-#   print(listing)
+@app.route("/", methods=['post', 'get'])
+def index():
+    message = ''
+    if "username" in session:
+        return redirect(url_for("logged_in"))
+    if request.method == "POST":
+        username = request.form.get("username")
+        
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
 
+        username_found = db.logins.find_one({"username": username})
+        if username_found:
+            message = 'This username already exists in database'
+            return render_template('index.html', message=message)
+        if password1 != password2:
+            message = 'Passwords should match!'
+            return render_template('index.html', message=message)
+        else:
+            hashed = bcrypt.hashpw(password2.encode('utf-8'), bcrypt.gensalt())
+            user_input = {'username': username, 'password': hashed}
+            db.logins.insert_one(user_input)
+            
+            user_data = db.logins.find_one({"username": username})
+            new_username = user_data['username']
+   
+            return render_template('logged_in.html', username=new_username)
+    return render_template('index.html')
+
+@app.route('/logged_in')
+def logged_in():
+    if "username" in session:
+        username = session["username"]
+        return render_template('logged_in.html', username=username)
+    else:
+        return redirect(url_for("login"))
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    message = 'Please login to your account'
+    if "username" in session:
+        return redirect(url_for("logged_in"))
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        username_found = db.logins.find_one({"username": username})
+        print(username)
+        if username_found:
+            username_val = username_found['username']
+            passwordcheck = username_found['password']
+            
+            if bcrypt.checkpw(password.encode('utf-8'), passwordcheck):
+                session["username"] = username_val
+                return redirect(url_for('logged_in'))
+            else:
+                if "username" in session:
+                    return redirect(url_for("logged_in"))
+                message = 'Wrong password'
+                return render_template('login.html', message=message)
+        else:
+            message = 'username not found'
+            return render_template('login.html', message=message)
+    return render_template('login.html', message=message)
+
+@app.route("/logout", methods=["POST", "GET"])
+def logout():
+    if "username" in session:
+        session.pop("username", None)
+        return render_template("logout.html")
+    else:
+        return render_template('index.html')
+    
 if __name__ == "__main__":
   # pass
   app.run(host='0.0.0.0')
